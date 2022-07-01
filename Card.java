dc2b7f2b828dd87d98e2ad7403c38f2891eaa60d
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2022-07-01 22:20:14 +0700

    orig codes

---
+++ Card.java
@@ -0,0 +1,68 @@
+package Game;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class Card extends JButton {
+	
+	
+	ImageIcon frontImage;
+	ImageIcon backImage;
+	boolean locked = false; 			
+	int id; 							
+	boolean activeIcon = false;
+	boolean matched = false; 
+	
+
+	
+	
+	
+	public Card(int num,ImageIcon on, ImageIcon arka)
+	{
+
+		this.frontImage = on;
+		this.backImage = arka;
+		this.id = num;
+//		boolean faceUp = false; 
+		 
+	
+
+	}
+	
+	
+	public boolean WhichSideIsOn()
+	{
+
+	    
+		return this.activeIcon;
+		 
+
+	}
+	
+	public void LockTheCard()
+	{
+
+		 this.locked= true;
+		 
+
+	}
+	
+	public void SetFrontIcon()
+	{
+        
+		 this.setIcon(this.frontImage) ;	 
+		 this.activeIcon= true;
+
+	}
+	
+	public void SetBackIcon()
+	{
+
+		 this.setIcon(this.backImage) ;	 
+		 this.activeIcon= false;
+
+	}
+		
+	
+
+}
---
+++ Game.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Game {
+	
+	
+	public static void main( String[] argv )
+	{
+		// make an instance of the main game class
+		GameHandler instance = new GameHandler();
+		// start a new game
+		instance.newGame();
+	}
+}
